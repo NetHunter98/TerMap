29be6a23eb29c666bcedc1968ce10c514e7cafa5
Tawakkal23 <55172998+Tawakkal23@users.noreply.github.com>
2019-09-13 17:29:02 +0800

    Add files via upload

---
+++ TerMap.sh
@@ -0,0 +1,21 @@
+#!/system/bin/sh
+
+clear
+
+figlet "Ter Map" | lolcat
+echo "#####@@@-_Tawakkal23_-@@@#####" | lolcat
+echo
+echo "=================================" | lolcat
+echo
+sleep 2
+echo "*#@*# KATAKAN PETA !!!" | lolcat
+echo
+sleep 1
+echo "*#@*# KATAKAN PETA !!!" | lolcat
+echo
+sleep 1
+echo "*#@*# KATAKAN PETA !!!" | lolcat
+echo
+sleep 3
+
+telnet mapscii.me
